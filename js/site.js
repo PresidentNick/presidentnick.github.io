8f0180bd8c93ce150f7fe945add0ccd3b4f790e2
Nick <nickpereira.np@gmail.com>
2015-02-13 02:05:56 -0600

    initial deploy to live

--- js/site.js
+++ js/site.js
@@ -28,16 +28,18 @@ $(document).ready(function() {
 
   function smoothScroll(e) {
     e.preventDefault();
-    $(document).off("scroll");
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-40
-    }, 0, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
+    $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-40
+        }, 1000);
+        return false;
+      }
+    }
+  });
   }
 
   function openPopover(e) {
